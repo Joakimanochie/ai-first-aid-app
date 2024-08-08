9fa785cbdab23b06ccad16f57892102fd57e8ba6
Joakimanochie <anochietobe@gmail.com>
2024-08-08 19:11:16 +0100

    adding clerk auth

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,7 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#D5211B",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
